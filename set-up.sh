771c70cf1413495fc794614bd8a9093eea558c2a
Peter McHale <peter.thomas.mchale@gmail.com>
2020-08-20 18:09:11 -0600

    fix change directory problem

--- set-up.sh
+++ set-up.sh
@@ -6,12 +6,14 @@ PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 chmod u+x *.sh
 
+current_directory=$PWD
 mkdir $HOME/node
 cd $HOME/node
 wget https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz
 tar -xf node-v12.18.3-linux-x64.tar.xz
 ln -s $PWD/node-v12.18.3-linux-x64/bin/node $HOME/bin/node
 ln -s $PWD/node-v12.18.3-linux-x64/bin/npm $HOME/bin/npm
+cd ${current_directory}
 
 pip install cyvcf2
 
