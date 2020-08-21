4aa76ff03a9870f087d13614226e5115590117ff
Peter McHale <peter.thomas.mchale@gmail.com>
2020-08-21 17:20:49 -0600

    xxx

--- set-up.sh
+++ set-up.sh
@@ -4,10 +4,10 @@ set -o nounset
 set -o xtrace
 PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-node_version="v12.18.3"
-
 root_directory="$PWD" 
 
+# this section assumes that this script is being run on the cluster
+node_version="v12.18.3"
 curl --remote-name "https://nodejs.org/dist/${node_version}/node-${node_version}-linux-x64.tar.xz"
 tar -xf "node-${node_version}-linux-x64.tar.xz"
 rm "node-${node_version}-linux-x64.tar.xz"
@@ -28,6 +28,7 @@ expand_path callSets.json
 cd "${root_directory}/create-screenshots" && npm install
 cd "${root_directory}/web-app" && npm install
 
+# this section assumes that this script is being run on the cluster
 set +x 
 echo "***********************" 
 echo "add the following to your PATH environment variable in ~/.bash_profile:"
