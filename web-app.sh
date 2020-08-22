e9909d60b6d4761e8f6197bdc6f0f91d2163d7d1
Peter McHale <peter.thomas.mchale@gmail.com>
2020-08-22 10:18:28 -0600

    node path in create-screenshots

--- web-app.sh
+++ web-app.sh
@@ -1,3 +1,10 @@
+root_directory="$PWD"
+# no need to export PATH since it is already in the environment: https://unix.stackexchange.com/a/26059/406037
+PATH="${root_directory}/node/bin:$PATH"
+
 cd web-app 
+which node 
+echo $PATH
+exit 1
 npm run serve
 
