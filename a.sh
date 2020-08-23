070f7c980574277e5f0e6cb20432e5585f3facc8
Peter McHale <peter.thomas.mchale@gmail.com>
2020-08-23 16:31:50 -0600

    use correct node for serve.js

--- a.sh
+++ a.sh
@@ -1,7 +1,5 @@
 set -x 
 
-port="1000" 
-
 root_directory="$PWD"
 # no need to export PATH since it is already in the environment: https://unix.stackexchange.com/a/26059/406037
 PATH="${root_directory}/node/bin:$PATH"
@@ -10,5 +8,6 @@ cd web-app
 
 npm run build
 cp -r public/igv dist
+
 # node ~/igv-grid/create-screenshots/node_modules/http-server/bin/http-server
-npx serve --single --symlinks --listen ${port} dist
+node node_modules/serve/bin/serve.js --single --symlinks dist
